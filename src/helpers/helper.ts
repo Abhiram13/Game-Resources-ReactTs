a1d02e04c98c9797a88f2d1854ab69b30761e56e
Abhiram13 <abhiram131869@gmail.com>
2021-04-13 10:20:06 +0530

    changed login response object

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,30 +1,30 @@
 import {postMethod} from './interface';
 
 let request = (function() {
-     let server: string = 'http://localhost:5000';
+   let server: string = 'http://localhost:1996';
 
-     return {
-          post: function(url: string, data: object | string, func: postMethod): void {
-               let XHTTP = new XMLHttpRequest();
-               XHTTP.open('post', `${server}/${url}`, true);
-               XHTTP.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-               XHTTP.onreadystatechange = function() {
-                    console.log(this);
-                    if(this.readyState === 4 && this.status === 200) {
-                         func(XHTTP);
-                    }
-               };
-               XHTTP.send(JSON.stringify(data));
-          },
+   return {
+      post: function(url: string, data: object | string, func: postMethod): void {
+         let XHTTP = new XMLHttpRequest();
+         XHTTP.open('post', `${server}/${url}`, true);
+         XHTTP.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+         XHTTP.onreadystatechange = function() {
+            console.log(this);
+            if (this.readyState === 4 && this.status === 200) {
+               func(XHTTP);
+            }
+         };
+         XHTTP.send(JSON.stringify(data));
+      },
 
-          get: async function <T>(url: string): Promise<T> {
-               const response = await fetch(`${server}/${url}`);
-               const body = await response.json();
+      get: async function <T>(url: string): Promise<T> {
+         const response = await fetch(`${server}/${url}`);
+         const body = await response.json();
 
-               if(response.status !== 200) alert(body.message);
-               return body;
-          }
-     };
+         if (response.status !== 200) alert(body.message);
+         return body;
+      }
+   };
 })();
 
 export default request;
--- src/workflow/auth/auth.tsx
+++ src/workflow/auth/auth.tsx
@@ -3,71 +3,72 @@ import {withRouter, RouteComponentProps} from 'react-router-dom';
 import Login from './login';
 import SignIn from './signin';
 import request from '../../helpers/helper';
-import {AuthoriseState} from '../../helpers/interface';
+import {AuthoriseState, ILoginResponse} from '../../helpers/interface';
 
 class Auth extends React.Component<RouteComponentProps, AuthoriseState> {
-     state: AuthoriseState = {
-          userExist: true,
-          loginData: '',
-          signinData: '',
-     };
+   state: AuthoriseState = {
+      userExist: true,
+      loginData: '',
+      signinData: '',
+   };
 
-     getLoginCredentials = (credentials: object): void => {
-          this.setState({loginData: credentials}, () => {
-               request.post('login.js', this.state.loginData, (XHTTP: XMLHttpRequest) => {
-                    const response = JSON.parse(XHTTP.responseText);
-                    if(response.access) {
-                         window.location.assign(`/${response.document[0]._id}/home`);
-                    } else {
-                         alert('Incorrect UserName or Password');
-                    }
-               });
-          });
-     };
+   getLoginCredentials = (credentials: object): void => {
+      this.setState({loginData: credentials}, () => {
+         request.post('login', this.state.loginData, (XHTTP: XMLHttpRequest) => {
+            const response: ILoginResponse = JSON.parse(XHTTP.responseText);
+            console.log(response);
+            if (response.token) {
+               window.location.assign(`/${response.user._id}/home`);
+            } else {
+               alert('Incorrect UserName or Password');
+            }
+         });
+      });
+   };
 
-     getSignUpCredentials = (credentials: object) => {
-          this.setState({signinData: credentials}, () => {
-               request.post('signIn.js', this.state.signinData, (XHTTP: XMLHttpRequest) => {
-                    const response = JSON.parse(XHTTP.responseText);
-                    alert((response.status) ? 'User has been Registered Successfully' : 'User has already been Registered');
-                    this.setState({userExist: true});
-               });
-          });
-     };
+   getSignUpCredentials = (credentials: object) => {
+      this.setState({signinData: credentials}, () => {
+         request.post('signIn.js', this.state.signinData, (XHTTP: XMLHttpRequest) => {
+            const response = JSON.parse(XHTTP.responseText);
+            alert((response.status) ? 'User has been Registered Successfully' : 'User has already been Registered');
+            this.setState({userExist: true});
+         });
+      });
+   };
 
-     createUser = (a: boolean): void => {
-          this.setState({userExist: a});
-     };
+   createUser = (a: boolean): void => {
+      this.setState({userExist: a});
+   };
 
-     existingUser = (a: boolean): void => {
-          this.setState({userExist: a});
-     };
+   existingUser = (a: boolean): void => {
+      this.setState({userExist: a});
+   };
 
-     render(): React.ReactNode {
-          return (
-               <Fragment>
-                    <div className="row" style={{height: '-webkit-fill-available'}}>
-                         <section className="col-sm-6 p-0 m-0 position-relative">
-                              <div className="col-sm-8 p-0 m-0 position-absolute" style={{top: '40%', left: '10%'}}>
-                                   <h1 className="m-0" style={{fontSize: '3.4rem'}}><strong className="text-white">Welcome User,</strong></h1>
-                                   <p className="m-0 p-0 text-white">
-                                        {this.state.userExist ? 'Please Login to your Account' : 'Please Create your Account'}
-                                   </p>
-                              </div>
-                         </section>
-                         <section className="col-sm-6 p-0 m-0 position-relative">
-                              <div className="col-sm-7 p-0 mx-auto position-absolute bg-white box_shadow" style={{top: '17%', left: '25%', borderRadius: '18px', height: '450px'}}>
-                                   {
-                                        this.state.userExist
-                                             ? <Login credentials={this.getLoginCredentials} newUser={this.createUser} />
-                                             : <SignIn create={this.getSignUpCredentials} exist={this.existingUser} />
-                                   }
-                              </div>
-                         </section>
-                    </div>
-               </Fragment>
-          );
-     }
+   render(): React.ReactNode {
+      return (
+         <Fragment>
+            <div className="row" style={{height: '-webkit-fill-available'}}>
+               <section className="col-sm-6 p-0 m-0 position-relative">
+                  <div className="col-sm-8 p-0 m-0 position-absolute" style={{top: '40%', left: '10%'}}>
+                     <h1 className="m-0" style={{fontSize: '3.4rem'}}><strong className="text-white">Welcome User,</strong></h1>
+                     <p className="m-0 p-0 text-white">
+                        {this.state.userExist ? 'Please Login to your Account' : 'Please Create your Account'}
+                     </p>
+                  </div>
+               </section>
+               <section className="col-sm-6 p-0 m-0 position-relative">
+                  <div className="col-sm-7 p-0 mx-auto position-absolute bg-white box_shadow" style={{top: '17%', left: '25%', borderRadius: '18px', height: '450px'}}>
+                     {
+                        this.state.userExist
+                           ? <Login credentials={this.getLoginCredentials} newUser={this.createUser} />
+                           : <SignIn create={this.getSignUpCredentials} exist={this.existingUser} />
+                     }
+                  </div>
+               </section>
+            </div>
+         </Fragment>
+      );
+   }
 }
 
 export default withRouter(Auth);
